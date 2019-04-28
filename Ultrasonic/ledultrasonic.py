678bc3a4058a7b68eb0542478f3aa383a6f304ed
David Medina <dameos1@gmail.com>
2019-04-28 07:06:36 +0000

    Adding ultrasonic in main

---
+++ Ultrasonic/ledultrasonic.py
@@ -0,0 +1,66 @@
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+from time import sleep
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial)
+centre = (3, 3, 4, 4)
+
+def display_front(level, canv):
+    __display(2, level, canv)
+
+def display_right(level, canv):
+    __display(3, level, canv)
+
+def display_left(level, canv):
+    __display(1, -level, canv)
+
+def display_back(level, canv):
+    __display(0, -level, canv)
+
+def __display(pos, level, canv):
+    centre_list = list(centre)
+    centre_list[pos] = centre[pos] + level
+    with canv as draw:
+        draw.rectangle(tuple(centre_list), outline='red', fill=None)
+
+def display_diag_pos(level, canv):
+    pos = centre[3]
+    centre_list = list(centre)
+    with canv as draw:
+        for i in range(pos, pos + level + 1):
+            draw.point((i, i), fill='red')
+
+def display_diag_neg(level, canv):
+    pos = 4
+    with canv as draw:
+        for i in range(1, level + 1):
+            draw.point((pos + i, (pos - 1) - i), fill='red')
+
+def create_canvas():
+    return canvas(device)
+
+def decode_sensor(sensor, level, canv):
+    if sensor == 0:
+        display_front(level, canv)
+    if sensor == 1:
+        display_right(level, canv)
+    if sensor == 2:
+        display_left(level, canv)
+    if sensor == 3:
+        display_back(level, canv)
+    if sensor == 4:
+        display_diag_pos(level, canv)
+    if sensor == 5:
+        display_diag_neg(level, canv)
+
+def from_distance_to_level(distance):
+    if distance > 30:
+        return 0
+    if distance <= 30 and distance > 20:
+        return 1
+    if distance <= 20 and distance > 10:
+        return 2
+    if distance <= 10:
+        return 3
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+import sys
+from Ultrasonic import ledultrasonic as led
 from Ultrasonic.ultrasonic import Ultrasonic
 from Motors.motors import Motors
 from time import sleep
 
+
 sensor1 = Ultrasonic(trig=18, echo=23)
 sensor2 = Ultrasonic(trig=24, echo=25)
-sensor3 = Ultrasonic(trig=8, echo=7)
+sensor3 = Ultrasonic(trig=13, echo=7)
 sensor4 = Ultrasonic(trig=12, echo=16)
 sensor5 = Ultrasonic(trig=20, echo=21)
 sensor6 = Ultrasonic(trig=19, echo=26)
 
-utlrasonic_sensors = [sensor1, sensor2, sensor3, sensor4, sensor5, sensor6]
+ultrasonic_sensors = [sensor1, sensor2, sensor3, sensor4, sensor5]
+
+canv = led.create_canvas()
 
 while 1:
-    for i, sensor in enumerate(utlrasonic_sensors):
-        print('Sensor' + str(i + 1) + ' : ' + str(sensor.sense_distance()))
-    sleep(0.2)
+    distances = list(map(lambda x: x.sense_distance(), ultrasonic_sensors))
+    for i, distance in enumerate(distances):
+        print('Sensor' + str(i + 1) + ' : ' + str(distance))
+        led.decode_sensor(i, led.from_distance_to_level(distance), canv)
+    canv = led.create_canvas()
